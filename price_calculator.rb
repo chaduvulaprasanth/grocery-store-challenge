844d32fc1b69ac4f18dd9a3defa64df50ee603c8
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-07-24 20:47:20 +0530

    created price table & updated data

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,3 +1,20 @@
 class Price_calculator
-
+  def initialize
+    @pricing_table = {  
+      "milk" => {  
+        "price" => 3.97, 
+        "sale" => { "qun" => 2, "cost" => 5 }
+        },
+      "bread" => { 
+        "price" => 2.17, 
+        "sale" => { "qun" => 3, "cost" => 6 }
+      },
+      "banana" => { 
+        "price" => 0.99 
+      },
+      "apple" => { 
+        "price" => 0.89
+      }
+    }
+  end
 end
