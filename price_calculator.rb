6045f8801b0b2f1cf63823505f410145827cacda
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-07-24 20:54:51 +0530

    method to recieve and sort orders

--- price_calculator.rb
+++ price_calculator.rb
@@ -16,5 +16,15 @@ class Price_calculator
         "price" => 0.89
       }
     }
+    @order = Hash.new(0)
+  end
+
+  def calculate(items)
+    items.each{ |item| @order[item.downcase] += 1 if @pricing_table[item.downcase]}
   end
 end
+
+price_calculator = Price_calculator.new
+puts "Please enter all the items purchased separated by a comma"
+items = gets.chomp.split(",")
+price_calculator.calculate(items) if items
